da3830c0c053d84044fdeae4d490b4a0270a3080
Wallace Maxters <wallacemaxters@gmail.com>
2017-07-28 13:47:49 -0300

    new tests added

--- test/MessageQueueTest.php
+++ test/MessageQueueTest.php
@@ -45,17 +45,53 @@ class MessageQueueTest extends TestCase
     public function testReceive()
     {   
 
-        $this->mq->send(5, ['hello' => 'world']);
+        $this->mq->send(2, ['hello' => 'world']);
 
         // Receive
 
-        list ($message, $type) = $this->mq->receive(5, 4096, true, null);
+        list ($message, $type) = $result = $this->mq->receive();
 
         $this->assertEquals(['hello' => 'world'], $message);
 
+        $this->assertEquals($type, 2);
+
+        $this->assertEquals($result['message'], ['hello' => 'world']);
+
+        $this->assertEquals($result['type'], 2);
+
         $this->mq->destroy();
+
     }
 
+
+    public function testReceiveRaw()
+    {
+        $this->mq->sendRaw(5, 'SysV');
+
+        $result = $this->mq->receiveRaw();
+
+        $this->assertEquals($result['message'], 'SysV');
+
+        $this->assertEquals($result['type'], 5);
+
+        $this->mq->destroy();
+
+    }
     
+
+    public function testReceiveJson()
+    {
+        $this->mq->sendJson(15, ['hello' => 'Wallace']);
+
+        $result = $this->mq->receiveJson();
+
+        $this->assertEquals(['hello' => 'Wallace'], $result['message']);
+
+        $this->assertEquals(['hello' => 'Wallace'], $result[0]);
+
+        $this->assertEquals(15, $result['type']);
+
+        $this->assertEquals(15, $result[1]);
+    }
 
 }
